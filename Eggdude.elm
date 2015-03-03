747cdf88d78f49e15ad097b94342d4f392fc4fe1
Daniel Howlett <dan@dancras.co.uk>
2015-03-03 02:10:44 +0100

    Experimenting with a follower

--- Eggdude.elm
+++ Eggdude.elm
@@ -10,7 +10,7 @@ import Window
 
 type alias Positioned p = { p | x : Float, y : Float }
 
-type alias Movable m = { m | vx: Float, vy: Float, ax: Float, ay: Float }
+type alias Movable m = Positioned({ m | vx: Float, vy: Float, ax: Float, ay: Float })
 
 type alias Wall = Positioned(
     { w : Float
@@ -18,11 +18,15 @@ type alias Wall = Positioned(
     , color : Color
     })
 
-type alias Player = Movable(Positioned({}))
+type alias Player = Movable(
+    { facingX : Int,
+      facingY : Int
+    })
 
 type alias World =
     { walls : List Wall
     , player : Player
+    , follower : Movable({})
     }
 
 type alias Camera = Positioned({ boundX : Float })
@@ -35,7 +39,8 @@ world =
         [ { x = -500, y = 100, w = 800, h = 30, color = rgb 200 40 40 }
         , { x = 600, y = 200, w = 600, h = 30, color = rgb 40 200 40 }
         ]
-    , player = { x = 0, y = 0, vx = 0, vy = 0, ax = 0, ay = 0 }
+    , player = { x = 0, y = 0, vx = 0, vy = 0, ax = 0, ay = 0, facingX = 1, facingY = 0 }
+    , follower = { x = 100, y = 0, vx = 0, vy = 0, ax = 0, ay = 0 }
     }
 
 camera : Camera
@@ -46,11 +51,37 @@ updateWorld (delta, keys) world =
     { world |
         player <- world.player
             |> updatePlayerAcceleration keys
-            |> resistanceDueToVelocity
-            |> updatePlayerVelocity
-            |> updatePlayerPosition delta
-            |> Debug.watch "player"
+            |> updatePlayerFacing keys
+            |> resistanceDueToVelocity 0.02
+            |> updateVelocity
+            |> updatePosition delta
+            |> Debug.watch "player",
+        follower <- world.follower
+            |> updateFollowerAcceleration world.player (firstFollowerTarget world.player)
+            |> resistanceDueToVelocity 0.015
+            |> updateVelocity
+            |> updatePosition delta
+            |> Debug.watch "follower"
     }
+
+firstFollowerTarget : Player -> (Float, Float)
+firstFollowerTarget player =
+    ( if | player.facingX > 0 -> player.x - 80
+         | player.facingX < 0 -> player.x + 80
+         | otherwise -> player.x
+    , if | player.facingY > 0 -> player.y - 80
+         | player.facingY < 0 -> player.y + 80
+         | otherwise -> player.y
+    )
+
+updateFollowerAcceleration : Player -> (Float, Float) -> Movable(m) -> Movable(m)
+updateFollowerAcceleration player (x, y) follower =
+    let a = sqrt ((player.x - follower.x)^2 + (player.y - follower.y)^2)
+    in
+        { follower |
+            ax <- (x - follower.x) / (200 - a),
+            ay <- (y - follower.y) / (200 - a)
+        }
 
 updatePlayerAcceleration : Keys -> Player -> Player
 updatePlayerAcceleration keys player =
@@ -66,25 +97,34 @@ updatePlayerAcceleration keys player =
                      | otherwise -> max -a <| min a <| -player.vy
         }
 
-resistanceDueToVelocity : Player -> Player
-resistanceDueToVelocity player =
-    { player |
-        ax <- player.ax - (abs player.vx) * player.vx * 0.02,
-        ay <- player.ay - (abs player.vy) * player.vy * 0.02
+resistanceDueToVelocity : Float -> Movable(m) -> Movable(m)
+resistanceDueToVelocity coefficient subject =
+    { subject |
+        ax <- subject.ax - (abs subject.vx) * subject.vx * coefficient,
+        ay <- subject.ay - (abs subject.vy) * subject.vy * coefficient
     }
 
-updatePlayerVelocity : Player -> Player
-updatePlayerVelocity player =
-    { player |
-        vx <- player.vx + player.ax,
-        vy <- player.vy + player.ay
+updateVelocity : Movable(m) -> Movable(m)
+updateVelocity subject =
+    { subject |
+        vx <- subject.vx + subject.ax,
+        vy <- subject.vy + subject.ay
     }
 
-updatePlayerPosition : Float -> Player -> Player
-updatePlayerPosition delta player =
+updatePosition : Float -> Movable(m) -> Movable(m)
+updatePosition delta subject =
+    { subject |
+        x <- subject.x + delta * subject.vx,
+        y <- subject.y + delta * subject.vy
+    }
+
+updatePlayerFacing : Keys -> Player -> Player
+updatePlayerFacing keys player =
     { player |
-        x <- player.x + delta * player.vx,
-        y <- player.y + delta * player.vy
+        facingX <- if | keys.x == 0 && keys.y == 0 -> player.facingX
+                      | otherwise -> keys.x,
+        facingY <- if | keys.x == 0 && keys.y == 0 -> player.facingY
+                      | otherwise -> keys.y
     }
 
 movePositioned : (Float, Float) -> Positioned(p) -> Positioned(p)
@@ -98,7 +138,8 @@ worldThroughCamera : Camera -> World -> World
 worldThroughCamera camera world =
     { world |
         walls <- map (movePositioned (camera.x, camera.y)) world.walls,
-        player <- movePositioned (camera.x, camera.y) world.player
+        player <- movePositioned (camera.x, camera.y) world.player,
+        follower <- movePositioned (camera.x, camera.y) world.follower
     }
 
 updateCamera : World -> Camera -> Camera
@@ -118,12 +159,15 @@ drawWall wall =
         |> move (wall.x, wall.y)
 
 view : (Int, Int) -> World -> Element
-view (w', h') {walls, player} =
+view (w', h') {walls, player, follower} =
     let (w, h) = (toFloat w', toFloat h')
     in
         collage w' h' <| append
             [ rect w h
                 |> filled (rgb 240 250 230)
+            , circle 20
+                |> filled (rgb 0 0 100)
+                |> move (follower.x, follower.y)
             , circle 50
                 |> filled (rgb 0 0 0)
                 |> move (player.x, player.y)
