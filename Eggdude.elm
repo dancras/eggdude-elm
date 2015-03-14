a81150629210e691576602ad8fda924e247e1207
Daniel Howlett <dan@dancras.co.uk>
2015-03-14 20:39:49 +0100

    Tweak follower acceleration to behave like player. Add more followers.

--- Eggdude.elm
+++ Eggdude.elm
@@ -26,7 +26,7 @@ type alias Player = Movable(
 type alias World =
     { walls : List Wall
     , player : Player
-    , follower : Movable({})
+    , followers : List( Movable({}) )
     }
 
 type alias Camera = Positioned({ boundX : Float })
@@ -40,7 +40,11 @@ world =
         , { x = 600, y = 200, w = 600, h = 30, color = rgb 40 200 40 }
         ]
     , player = { x = 0, y = 0, vx = 0, vy = 0, ax = 0, ay = 0, facingX = 1, facingY = 0 }
-    , follower = { x = 100, y = 0, vx = 0, vy = 0, ax = 0, ay = 0 }
+    , followers = [ { x = 100, y = 0, vx = 0, vy = 0, ax = 0, ay = 0 }
+                  , { x = 200, y = 0, vx = 0, vy = 0, ax = 0, ay = 0 }
+                  , { x = 300, y = 0, vx = 0, vy = 0, ax = 0, ay = 0 }
+                  , { x = 400, y = 0, vx = 0, vy = 0, ax = 0, ay = 0 }
+                  ]
     }
 
 camera : Camera
@@ -50,19 +54,39 @@ updateWorld : (Float, Keys) -> World -> World
 updateWorld (delta, keys) world =
     { world |
         player <- world.player
-            |> updatePlayerAcceleration keys
+            |> updateAcceleration keys
             |> updatePlayerFacing keys
             |> resistanceDueToVelocity 0.02
             |> updateVelocity
             |> updatePosition delta
             |> Debug.watch "player",
-        follower <- world.follower
-            |> updateFollowerAcceleration world.player (firstFollowerTarget world.player)
-            |> resistanceDueToVelocity 0.015
-            |> updateVelocity
-            |> updatePosition delta
-            |> Debug.watch "follower"
+        followers <- world.followers
+            |> map2 updateAcceleration (followerKeys (toPositioned world.player) (map toPositioned world.followers))
+            |> map (resistanceDueToVelocity 0.015)
+            |> map updateVelocity
+            |> map (updatePosition delta)
+            |> Debug.watch "followers"
     }
+
+toPositioned : Positioned(a) -> Positioned({})
+toPositioned p = { x = p.x, y = p.y }
+
+followerKeys : Positioned a -> List(Positioned a) -> List(Keys)
+followerKeys firstTarget followers =
+    map2 followerAI (firstTarget :: followers) followers
+
+followerAI : Positioned(a) -> Positioned(b) -> Keys
+followerAI target follower =
+    let distance = sqrt ((target.x - follower.x)^2 + (target.y - follower.y)^2)
+    in
+        if | distance > 100 -> { x = if | target.x - follower.x > 5 -> 1
+                                        | target.x - follower.x < -5 -> -1
+                                        | otherwise -> 0
+                               , y = if | target.y - follower.y > 5 -> 1
+                                        | target.y - follower.y < -5 -> -1
+                                        | otherwise -> 0
+                               }
+           | otherwise -> { x = 0, y = 0 }
 
 firstFollowerTarget : Player -> (Float, Float)
 firstFollowerTarget player =
@@ -83,18 +107,18 @@ updateFollowerAcceleration player (x, y) follower =
             ay <- (y - follower.y) / (200 - a)
         }
 
-updatePlayerAcceleration : Keys -> Player -> Player
-updatePlayerAcceleration keys player =
+updateAcceleration : Keys -> Movable(m) -> Movable(m)
+updateAcceleration keys subject =
     let a = if | keys.x /= 0 && keys.y /= 0 -> 1
                | otherwise -> 2
     in
-        { player |
+        { subject |
             ax <- if | keys.x > 0 -> a
                      | keys.x < 0 -> -a
-                     | otherwise -> max -a <| min a <| -player.vx,
+                     | otherwise -> max -a <| min a <| -subject.vx,
             ay <- if | keys.y > 0 -> a
                      | keys.y < 0 -> -a
-                     | otherwise -> max -a <| min a <| -player.vy
+                     | otherwise -> max -a <| min a <| -subject.vy
         }
 
 resistanceDueToVelocity : Float -> Movable(m) -> Movable(m)
@@ -139,7 +163,7 @@ worldThroughCamera camera world =
     { world |
         walls <- map (movePositioned (camera.x, camera.y)) world.walls,
         player <- movePositioned (camera.x, camera.y) world.player,
-        follower <- movePositioned (camera.x, camera.y) world.follower
+        followers <- map (movePositioned (camera.x, camera.y)) world.followers
     }
 
 updateCamera : World -> Camera -> Camera
@@ -158,21 +182,28 @@ drawWall wall =
         |> filled wall.color
         |> move (wall.x, wall.y)
 
+drawFollower : Movable(m) -> Form
+drawFollower follower =
+    circle 20
+        |> filled (rgb 0 0 100)
+        |> move (follower.x, follower.y)
+
 view : (Int, Int) -> World -> Element
-view (w', h') {walls, player, follower} =
+view (w', h') {walls, player, followers} =
     let (w, h) = (toFloat w', toFloat h')
     in
-        collage w' h' <| append
-            [ rect w h
-                |> filled (rgb 240 250 230)
-            , circle 20
-                |> filled (rgb 0 0 100)
-                |> move (follower.x, follower.y)
-            , circle 50
-                |> filled (rgb 0 0 0)
-                |> move (player.x, player.y)
+        collage w' h' <| concat
+            [
+                [ rect w h
+                    |> filled (rgb 240 250 230)
+                ]
+            ,   (map drawFollower followers)
+            ,   [ circle 50
+                    |> filled (rgb 0 0 0)
+                    |> move (player.x, player.y)
+                ]
+            ,   (map drawWall walls)
             ]
-            (map drawWall walls)
 
 input : Signal (Float, Keys)
 input =
